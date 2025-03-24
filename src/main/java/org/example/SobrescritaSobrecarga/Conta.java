2107e05af7e09d4d2768f0ac79cb0fa35cc0281e
EvaReboucas30 <140523579+EvaReboucas30@users.noreply.github.com>
2025-03-24 18:26:16 -0300

    Fazendo teste unitário.

---
+++ src/main/java/org/example/SobrescritaSobrecarga/Conta.java
@@ -0,0 +1,20 @@
+package org.example.SobrescritaSobrecarga;
+
+import lombok.Getter;
+
+
+@Getter
+public class Conta {
+    protected Double saldo;
+    private Double taxaOperacao;
+
+    public  Conta(Double saldo) {
+        this.saldo = saldo;
+        this.taxaOperacao = 0.45;
+
+    }
+
+    public void depositar(Double valor){
+        this.saldo += valor - taxaOperacao;
+    }
+}
